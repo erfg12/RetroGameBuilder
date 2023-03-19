b94d562c462e88f939f396f78373556fca6effc9
Jacob Fliss <newgmobile@gmail.com>
2023-03-19 00:36:54 -0400

    psvita builds

--- psvita/build.sh
+++ psvita/build.sh
@@ -1,4 +1,17 @@
-cmake -S. -Bbuild -DCMAKE_TOOLCHAIN_FILE=${VITASDK}/share/vita.toolchain.cmake -DCMAKE_BUILD_TYPE=Release
-cmake --build build
-cmake --install build
-make
+#!/bin/sh
+
+SCRIPTNAME='build.sh'
+RC=0
+if [ -n "${BASH:-}" ]; then
+  [ "${0}" = "${BASH_SOURCE}" ] || RC=1
+else
+  [ "$(basename -- "${0}")" = "${SCRIPTNAME}" ] || RC=1
+fi
+[ "${RC}" -eq 0 ] || {
+  printf -- 'Script cannot be sourced\n'
+  return 1 || exit 1
+}
+
+[ -d 'build' ] || mkdir build
+cd build
+cmake .. && make
